f1fc4c7352f1aa65c20480371e821c63e63fd6f0
tlw52836 <1660872260@qq.com>
2022-04-26 14:23:30 +0800

    2022.04.20

--- blog-api/src/main/java/com/tlw/blog/config/MybatisPlusConfig.java
+++ blog-api/src/main/java/com/tlw/blog/config/MybatisPlusConfig.java
@@ -2,14 +2,18 @@ package com.tlw.blog.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.tlw.blog.handler.LoginInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
 //扫包，将此包下的接口生成代理实现类，并且注册到spring容器中
 @MapperScan("com.tlw.blog.mapper")
 public class MybatisPlusConfig {
+
     //分页插件
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
@@ -17,4 +21,5 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
+
 }
